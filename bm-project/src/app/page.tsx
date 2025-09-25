7f0021d6a1b5c38b5fdeb6991ab34d0cd63b856e
Ziad <x6rbanx2@gmail.com>
2025-09-25 17:36:00 +0300

    test 2

--- bm-project/src/app/page.tsx
+++ bm-project/src/app/page.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-
-    بسم الله الرحمن الرحيم
+الوووووووووو
     </div>
   );
 }
